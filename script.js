ed210aee49ef95397f3493b2ffdbee7fbcb1f48c
Madranges Lucas <lucasmadranges06@gmail.com>
2023-03-14 10:23:05 +0100

    Ajout du nombre de coups

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-var valmax = prompt('Quelles est la valeur maximum souhaité ?');
+var valmax = prompt('Quelle est la valeur maximum souhaité ?');
 alert('La valeur aléatoire sera donc compris entre 0 et '+valmax);
 alert('Prêt ?');
 var valaleatoire = Math.round(Math.random()*valmax);
 var valutilisateur = 0;
+var nombrecoup = 0;
 while(valaleatoire != valutilisateur)
 {
     valutilisateur = prompt('Choisissez une valeur.');
     if(valutilisateur > valaleatoire)
     {
         alert('La valeur aléatoire est inférieur.');
+        nombrecoup++;
     }
     else if(valutilisateur < valaleatoire)
     {
         alert('La valeur aléatoire est supérieur.');
+        nombrecoup++;
     }
     else if(valutilisateur == valaleatoire)
     {
-        alert('Félicitation, vous avez trouvé le juste prix. La réponse était '+valaleatoire);
+        alert('Félicitation, vous avez trouvé le juste prix. La réponse était '+valaleatoire+". Vous avez trouvé la valeur en "+nombrecoup+' coup(s).');
     }
 }
 var choix = prompt('Recommencer une partie ? (O/N)');
-if(choix = 'N')
+if(choix == 'O')
+{
+    location.reload();
+}
+else if(choix == 'N')
 {
     alert("Merci d'avoir joué.");
 }
