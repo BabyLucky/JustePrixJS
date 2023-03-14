6b162ebcc5ffb288c3224660ecda5b088df71081
Madranges Lucas <lucasmadranges06@gmail.com>
2023-03-14 10:51:30 +0100

    Ajout d'un audio et d'un bouton pour lancer le jeu

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-var valmax = prompt('Quelle est la valeur maximum souhaité ?');
-alert('La valeur aléatoire sera donc compris entre 0 et '+valmax);
-alert('Prêt ?');
-var valaleatoire = Math.round(Math.random()*valmax);
-var valutilisateur = 0;
-var nombrecoup = 0;
-while(valaleatoire != valutilisateur)
+function loadgame()
 {
-    valutilisateur = prompt('Choisissez une valeur.');
-    if(valutilisateur > valaleatoire)
+    var valmax = prompt('Quelle est la valeur maximum souhaité ?');
+    alert('La valeur aléatoire sera donc compris entre 0 et '+valmax);
+    alert('Prêt ?');
+    var valaleatoire = Math.round(Math.random()*valmax);
+    var valutilisateur = 0;
+    var nombrecoup = 0;
+    while(valaleatoire != valutilisateur)
     {
-        alert('La valeur aléatoire est inférieur.');
-        nombrecoup++;
+        valutilisateur = prompt('Choisissez une valeur.');
+        if(valutilisateur > valaleatoire)
+        {
+            alert('La valeur aléatoire est inférieur.');
+            nombrecoup++;
+        }
+        else if(valutilisateur < valaleatoire)
+        {
+            alert('La valeur aléatoire est supérieur.');
+            nombrecoup++;
+        }
+        else if(valutilisateur == valaleatoire)
+        {
+            alert('Félicitation, vous avez trouvé le juste prix. La réponse était '+valaleatoire+". Vous avez trouvé la valeur en "+nombrecoup+' coup(s).');
+        }
     }
-    else if(valutilisateur < valaleatoire)
+    var choix = prompt('Recommencer une partie ? (O/N)');
+    if(choix == 'O')
     {
-        alert('La valeur aléatoire est supérieur.');
-        nombrecoup++;
+        location.reload();
     }
-    else if(valutilisateur == valaleatoire)
+    else if(choix == 'N')
     {
-        alert('Félicitation, vous avez trouvé le juste prix. La réponse était '+valaleatoire+". Vous avez trouvé la valeur en "+nombrecoup+' coup(s).');
+        alert("Merci d'avoir joué.");
     }
-}
-var choix = prompt('Recommencer une partie ? (O/N)');
-if(choix == 'O')
-{
-    location.reload();
-}
-else if(choix == 'N')
-{
-    alert("Merci d'avoir joué.");
-}
-else if(choix != 'N' && choix != 'O')
-{
-    alert("Ceci n'est pas une réponse");
+    else if(choix != 'N' && choix != 'O')
+    {
+        alert("Ceci n'est pas une réponse");
+    }
 }
