c3c1a5d0f2fc8222185147ea28ccc6c7d790a04c
Madranges Lucas <lucasmadranges06@gmail.com>
2023-03-19 12:07:39 +0100

    Ajout du mode 2 joueurs

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ function playaudio()
     audio.play();
 }
 
+function Modejoueur1()
+{
+    const Mode = 1;
+    loadgame(Mode);
+}
+
+function Modejoueur2()
+{
+    const Mode = 2;
+    loadgame(Mode);
+}
+
 function stopmp3()
 {
     const audio = document.getElementById('audiomp3');
@@ -16,48 +28,63 @@ function volumemoins()
     audio.volume = 0.5;
 }
 
-function loadgame()
+function loadgame(Mode)
 {
-    playaudio();
-
-    var valmax = prompt('Quelle est la valeur maximum souhaité ?');
-    alert('La valeur aléatoire sera donc compris entre 0 et '+valmax);
-    alert('Prêt ?');
-    var valaleatoire = Math.round(Math.random()*valmax);
-    var valutilisateur = 0;
-    var nombrecoup = 0;
-    while(valaleatoire != valutilisateur)
+    const ModeJoueurChoisi = Mode;
+    if(ModeJoueurChoisi == 1)
     {
-        valutilisateur = prompt('Choisissez une valeur.');
-        if(valutilisateur > valaleatoire)
+        alert('Mode 1 Joueur choisi');
+        const valmax = prompt('Quelle est la valeur maximum souhaité ?');
+        alert('La valeur aléatoire sera donc compris entre 0 et '+valmax);
+        alert('Prêt ?');
+        const valaleatoire = Math.round(Math.random()*valmax);
+        var valutilisateur = 0;
+        var nombrecoup = 0;
+        while(valaleatoire != valutilisateur)
         {
-            alert('La valeur aléatoire est inférieur.');
-            nombrecoup++;
-        }
-        else if(valutilisateur < valaleatoire)
-        {
-            alert('La valeur aléatoire est supérieur.');
-            nombrecoup++;
-        }
-        else if(valutilisateur == valaleatoire)
-        {
-            alert('Félicitation, vous avez trouvé le juste prix. La réponse était '+valaleatoire+". Vous avez trouvé la valeur en "+nombrecoup+' coup(s).');
+            valutilisateur = prompt('Choisissez une valeur.');
+            if(valutilisateur > valaleatoire)
+            {
+                alert('La valeur aléatoire est inférieur.');
+                nombrecoup++;
+            }
+            else if(valutilisateur < valaleatoire)
+            {
+                alert('La valeur aléatoire est supérieur.');
+                nombrecoup++;
+            }
+            else if(valutilisateur == valaleatoire)
+            {
+                alert('Félicitation, vous avez trouvé le juste prix. La réponse était '+valaleatoire+". Vous avez trouvé la valeur en "+nombrecoup+' coup(s).');
+            }
         }
     }
-    var choix = prompt('Recommencer une partie ? (O/N)');
-    while((choix == 'O') || (choix == 'N'))
+    else if(ModeJoueurChoisi == 2)
     {
-        if(choix == 'O')
+        alert('Mode 2 joueur choisi');
+        const valtrouve = prompt('(Joueur 2) Quelle est la valeur à trouvé ?');
+        const valmax = prompt('(Joueur 2) Quelle est la valeur maximum ?');
+        alert('(Joueur 1) La valeur aléatoire sera donc compris entre 0 et '+valmax);
+        alert('Prêt Joueur 1 ?');
+        var valutilisateur = 0;
+        var nombrecoup = 0;
+        while(valtrouve != valutilisateur)
         {
-            location.reload();
-        }
-        else if(choix == 'N')
-        {
-            alert("Merci d'avoir joué.");
-        }
-        else if(choix != 'N' && choix != 'O')
-        {
-            alert("Ceci n'est pas une réponse");
+            valutilisateur = prompt('(Joueur 1) Choisissez une valeur.');
+            if(valutilisateur > valtrouve)
+            {
+                alert('(Joueur 1) La valeur aléatoire est inférieur.');
+                nombrecoup++;
+            }
+            else if(valutilisateur < valtrouve)
+            {
+                alert('(Joueur 1) La valeur aléatoire est supérieur.');
+                nombrecoup++;
+            }
+            else if(valutilisateur == valtrouve)
+            {
+                alert('Félicitation Joueur 1, vous avez trouvé le juste prix de Joueur 2. La réponse était '+valaleatoire+". Vous avez trouvé la valeur en "+nombrecoup+' coup(s).');
+            }
         }
     }
 }
